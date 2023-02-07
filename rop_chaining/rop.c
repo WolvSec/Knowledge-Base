c3dffcaf8cdbe1dbd493aa7187f8099368c2ea2f
qhdwight <qhdwight@gmail.com>
2023-02-07 17:35:58 -0500

    Add rop example

---
+++ rop_chaining/rop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>  // For puts and fgets
+#include <unistd.h> // For execve
+#include <stdlib.h>
+
+void access_vault(int code)
+{
+  if (code == 1337) {
+    puts("Access granted");
+    execve("/bin/sh", NULL, NULL);
+  } else {
+    puts("Access denied!");
+  }
+}
+
+int main()
+{
+  char input_buffer[16];
+  puts("Enter the password to access Santa Ono's secret vault:");
+
+  fgets(input_buffer, 32, stdin); // Nothing can go wrong here right?
+
+  puts("HAHA you thought! There was no password, you can NEVER get in >:)");
+
+  return EXIT_SUCCESS;
+}
