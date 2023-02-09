a1ea42dcf257ca158de9d2f5e797808f8c0121f0
qhdwight <qhdwight@gmail.com>
2023-02-08 19:30:16 -0500

    Make lore make more sense

--- buffer_overflow/buffer_overflow.c
+++ buffer_overflow/buffer_overflow.c
@@ -8,12 +8,19 @@ void access_vault()
   execve("/bin/sh", NULL, NULL);
 }
 
-int main()
+void get_password()
 {
   char input_buffer[16];
   puts("Enter the password to access Santa Ono's secret vault:");
 
-  fgets(input_buffer, 32, stdin); // Nothing can go wrong here right?
+  fgets(input_buffer, 64, stdin); // Nothing can go wrong here right?
+}
+
+int main()
+{
+  get_password();
+
+  puts("HAHA you thought! There was no password, you can NEVER get in >:)");
 
   return EXIT_SUCCESS;
 }
--- rop_chaining/rop_chaining.c
+++ rop_chaining/rop_chaining.c
@@ -4,19 +4,37 @@
 
 void access_vault(int code)
 {
-  if (code == 1337) {
+  if (code == 1337)
+  {
     execve("/bin/sh", NULL, NULL);
-  } else {
+  }
+  else
+  {
     puts("Access denied!");
   }
 }
 
-int main()
+int get_password()
 {
   char input_buffer[16];
   puts("Enter the password to access Santa Ono's secret vault:");
 
-  fgets(input_buffer, 64, stdin); // Nothing can go wrong here right?
+  fgets(input_buffer, 64, stdin);
+
+  return atoi(input_buffer);
+}
+
+int main()
+{
+  int password = get_password();
+
+  if (password >= 1000)
+  {
+    puts("Only authorized personnel can enter numbers greater than 1000!");
+    return EXIT_FAILURE;
+  }
+
+  access_vault(password);
 
   return EXIT_SUCCESS;
 }
