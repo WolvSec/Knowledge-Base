8da9053b2929cdfdda85c55991fce008e96e491c
qhdwight <qhdwight@gmail.com>
2023-02-12 23:01:12 -0500

    Update Dockerfile to not build unicorn from source since pip works now

---
+++ docker/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Builds the docker image for amd64 and arm64 and pushes it to docker hub
+
+docker buildx build --platform=linux/arm64,linux/amd64 --tag qdwight/wolvsec --push .
