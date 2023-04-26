967a4557b459df75eae044e273d8813c90b9e56d
qhdwight <qhdwight@gmail.com>
2023-04-25 20:47:59 -0400

    Update dockerfiles

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 
 # Builds the docker image for amd64 and arm64 and pushes it to docker hub
 
-docker buildx build --platform=linux/arm64,linux/amd64 --tag qdwight/wolvsec --push .
+docker buildx build --platform=linux/arm64,linux/amd64 --tag qdwight/wolvsec --push -f Dockerfile.debian .
